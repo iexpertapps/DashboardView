05117dab2822c88ea5ce9858f1086168ec6fb3f8
Syed Zia <muhammadhasaansyed@icloud.com>
2024-12-06 12:21:53 +0500

    Initial Commit

---
+++ DashboardView/DashboardViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashboardViewApp.swift
+//  DashboardView
+//
+//  Created by Syed Zia ur Rehman on 06/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct DashboardViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
